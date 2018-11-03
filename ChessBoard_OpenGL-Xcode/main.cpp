5370c6efbcb14b8d1ec538f17f2d4bf2b9bdb013
Josep <josep@mbp13-2.local>
2018-11-03 13:27:53 +0100

    Initial Commit

---
+++ ChessBoard_OpenGL-Xcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ChessBoard_OpenGL-Xcode
+//
+//  Created by Josep on 3/11/18.
+//  Copyright © 2018 Universitat de Lleida. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
